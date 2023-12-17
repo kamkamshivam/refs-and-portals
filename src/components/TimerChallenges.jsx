e176c95a11b4360e734ea4436f6bb0b1b07a5eae
Shivam Kamotra <shivamkamotra@gmail.com>
2023-12-18 02:15:23 +0530

    Timer stop functionality is added.

--- src/components/TimerChallenges.jsx
+++ src/components/TimerChallenges.jsx
@@ -1,22 +1,25 @@
-import React , {useState} from 'react';
+import React , {useState , useRef} from 'react';
+// let a = null;  this a will be shared by every component ** in app.jsx
 export default function TimerChallenge({title, targetTime}){
     const [timerStarted , setTimerStarted] = useState(false);
     const[timerExpired , setTimerExpired] = useState(false);
-
+    const timer = useRef(); // each component will get its individual useref
     function handleStart(){
-        setTimeout(()=>{
+         timer.current= setTimeout(()=>{  //setTimeout returns a pointer  which is stored in timer variable and then used by clearTimeout
             setTimerExpired(true)
         }, targetTime *1000);
         setTimerStarted(true);
     }
-    
+function handleStop(){
+    clearTimeout(timer.current);
+}
     return <section className="challenge">
         <h2>{title}</h2>
         {timerExpired&& <p>You Lost!</p>}
         <p className="challenge-time">
             {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
             {timerStarted ? 'Stop' : 'Start'} Challenge
         </button>
         <p className={timerStarted ? 'active' : undefined}></p>
